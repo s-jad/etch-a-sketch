6a34af23fdc395c8c2e4e2333d22d29c0ce4517c
s-jad <s_jad@web.de>
2023-06-26 13:51:51 +0200

    Feature: choose grid sizes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,40 @@
 // DOM Elements
-
+const gridContainer = document.getElementById("grid-container");
+const inputBox = document.getElementById("input-grid-size");
+const submitInputButton = document.getElementById("btn-make-grid");
 
 // FUNCTIONS
+function sizeGrid(inputSize) {
+    // Remove previous grid
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 
+    // Use inputSize to create inputSize^2 divs
+    for (let i = 0; i < inputSize * inputSize; i++) {
+        let child = document.createElement('div');
+        child.id = `child-${i + 1}`;
+        child.classList.add("sketch-board");
+        child.style.width = `min(${80 / inputSize}vw, ${80 / inputSize}vh)`;
+        child.style.height = `min(${80 / inputSize}vw, ${80 / inputSize}vh)`;
 
+        gridContainer.appendChild(child);
+    }
 
+    gridContainer.style.gridTemplateColumns = `repeat(${inputSize}, min(${80 / inputSize}vw, ${80 / inputSize}vh))`;
+    gridContainer.style.gap = `min(0.4rem, ${4 / inputSize}rem)`;
+    console.log(`calculated width of GTC: ${gridContainer.style.gridTemplateColumns}`);
+}
 
 // EVENT LISTENERS   
+inputBox.addEventListener('keydown', (key) => {
+    if (key.key === "Enter") {
+        let inputSize = parseInt(inputBox.value);
+        sizeGrid(inputSize);
+    }
+});
 
-console.log(
-    "hi!"
-);
+submitInputButton.addEventListener('click', () => {
+    let inputSize = parseInt(inputBox.value);
+    sizeGrid(inputSize);
+})
